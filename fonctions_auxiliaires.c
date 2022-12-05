ddf6ab9e411943b20a87ae209834d453101b78c3
kadem <hocine@DESKTOP-6KCKHON.localdomain>
2022-12-06 00:04:31 +0100

    joker_1

--- fonctions_auxiliaires.c
+++ fonctions_auxiliaires.c
@@ -8,102 +8,108 @@
 #include <sys/types.h>
 #include <dirent.h>
 
-
-
-char**  explode(char *str, const char *separators, int* taille)
+char **explode(char *str, const char *separators, int *taille)
 {
     int i = 0;
     int size = 0;
-    char* s = NULL;
-    char** res = NULL;
+    char *s = NULL;
+    char **res = NULL;
 
-            
     // res[0]  = malloc(1*sizeof(char));
     // res[0][0] = '\0';
 
-    //Cas chaine vide
-    if(strlen(str) == 0)
+    // Cas chaine vide
+    if (strlen(str) == 0)
     {
         *taille = 0;
         return NULL;
-    } 
+    }
 
-    //Séparer la chaine en plusieurs sous chaines :
-    char * strToken = strtok (str, separators);
-    while ( strToken != NULL ) 
+    // Séparer la chaine en plusieurs sous chaines :
+    char *strToken = strtok(str, separators);
+    while (strToken != NULL)
     {
         // On copie strToken dans une chaine de caractère s (pour avoir utiliser la taille exact)
-        if(!(s = malloc(strlen(strToken) + 1))) 
+        if (!(s = malloc(strlen(strToken) + 1)))
             perror("malloc");
-        if(snprintf(s, strlen(strToken)+1, "%s", strToken) < 0)
+        if (snprintf(s, strlen(strToken) + 1, "%s", strToken) < 0)
         {
             perror("explode snprintf error ");
             exit(1);
         }
         s[strlen(strToken)] = '\0';
-        //On ajoute la chaine de caractere s au tableau res
+        // On ajoute la chaine de caractere s au tableau res
         size += 1;
         res = realloc(res, size * sizeof(char *)); // +1
 
-        if(res == NULL) 
+        if (res == NULL)
             perror("fonction explode : realloc erreur ");
 
         res[i] = s;
-        //res[i+1][] = '\0';
+        // res[i+1][] = '\0';
         i++;
 
         // On demande le token suivant.
-        strToken = strtok ( NULL, separators );
+        strToken = strtok(NULL, separators);
     }
-    
-    if(!s)  
+
+    if (!s)
         free(s);
     free(strToken);
 
-    *taille = i;//ici on retourne la taille de res
+    *taille = i; // ici on retourne la taille de res
     return res;
 }
 
-void free_StingArrayArray(char **s,int taille)
-{   
-    for(int i = 0; i < taille ; i++)
+//free char**
+void free_StingArrayArray(char **s, int taille)
+{
+    for (int i = 0; i < taille; i++)
         free(s[i]);
 
     free(s);
 }
 
-int isIn(char *path, char *fic){
+//retourne 1 si un le fichier fic existe dans le repertoire path, 0 sinon
+int isIn(char *path, char *fic)
+{
     DIR *dir = opendir(path);
     struct dirent *entry;
 
-    while((entry = readdir(dir))){
-        if(strcmp(entry->d_name, fic) == 0){
+    while ((entry = readdir(dir)))
+    {
+        if (strcmp(entry->d_name, fic) == 0)
+        {
             closedir(dir);
             return 1;
         }
-        
     }
     closedir(dir);
     return 0;
-    
 }
- 
 
-void execCMD(char *cmd, char **args){
+//execute la commande cmd avec comme arguments args
+void execCMD(char *cmd, char **args)
+{
     pid_t pid = fork();
-    if(pid == -1){
+    if (pid == -1)
+    {
         perror("fork");
-    }else if(pid != 0){
+        exit(1);
+    }
+    else if (pid != 0)
+    {
         wait(NULL);
-    }else{
+    }
+    else
+    {
         execvp(cmd, args);
     }
-
 }
 
-
+//execute une commande externe 
 void commande_externe(char **tab, int taille)
-{   
+{
     // On recupere la variable d'environnement path et on lui cree une copie (car si on la modifie la var d'env se modifie)
     char *envPath = getenv("PATH");
     size_t taille_envPath = strlen(envPath);
@@ -113,62 +119,60 @@ void commande_externe(char **tab, int taille)
     snprintf(path, taille_envPath + 1, "%s", envPath);
     path[taille_envPath] = '\0';
 
-
     // On decoupe la variable path avec le separateur ":"
     int taillarrpath;
-    char **pathDecoupe = explode(path, ":", &taillarrpath); 
-    free(path); 
+    char **pathDecoupe = explode(path, ":", &taillarrpath);
+    free(path);
 
     // on parcout chaque élément de la variable PATH
     for (int i = 0; i < taillarrpath; i++)
-    {   
+    {
         // On verifie si chaque chemin du tableau pathDecoup a un fichier de nom la commande ecrite par l'utilisateur (tab[0])
         // Si non on essaye avec un autre chemin
-        if(isIn(pathDecoupe[i],tab[0]) == 0)
+        if (isIn(pathDecoupe[i], tab[0]) == 0)
             continue;
         else
-        {   
+        {
             size_t taille_elem = strlen(pathDecoupe[i]);
 
             // On cree un tableau de chaine de caratere pour la fonction exec qui doit ressembler à ça pour l'exemple "ls -l path" -> [/usr/bin/ls, -l, path, NULL]
             // Taille + 1 car le dernier element doit etre NULL
-            char **arguments_exec = malloc((taille + 1) * sizeof(char *)); 
+            char **arguments_exec = malloc((taille + 1) * sizeof(char *));
 
-            
-            char *cmd = malloc(taille_elem + 1 + /* taille du '/' */ + strlen(tab[0]) + 1);
-            if (cmd == NULL) perror("malloc");
-            snprintf(cmd, taille_elem+1, "%s", pathDecoupe[i]);
+            char *cmd = malloc(taille_elem + 1 + /* taille du '/' */ +strlen(tab[0]) + 1);
+            if (cmd == NULL)
+                perror("malloc");
+            snprintf(cmd, taille_elem + 1, "%s", pathDecoupe[i]);
             cmd[taille_elem] = '\0';
 
-            //equivalent de strcat(arg0,tab[0]) : 
+            // equivalent de strcat(arg0,tab[0]) :
             cmd[taille_elem] = '/';
-            snprintf(&cmd[taille_elem +1], strlen(tab[0])+1, "%s", tab[0]);
+            snprintf(&cmd[taille_elem + 1], strlen(tab[0]) + 1, "%s", tab[0]);
             int t = taille_elem + 1 + strlen(tab[0]);
             cmd[t] = '\0';
 
             // On complete le premier argument du tableau avec la commande
             arguments_exec[0] = malloc(strlen(cmd) + 1); //---------------------->fuite mem
-            snprintf(arguments_exec[0],strlen(cmd)+1, "%s", cmd);
+            snprintf(arguments_exec[0], strlen(cmd) + 1, "%s", cmd);
             arguments_exec[0][strlen(cmd)] = '\0';
-
 
             // On remplit le tableau pour les options
             for (int i = 1; i < taille; i++)
-            {   
+            {
                 arguments_exec[i] = malloc(strlen(tab[i]) + 1);
-                snprintf(arguments_exec[i],strlen(tab[i])+1, "%s", tab[i]);
+                snprintf(arguments_exec[i], strlen(tab[i]) + 1, "%s", tab[i]);
             }
             arguments_exec[taille] = NULL;
 
             execCMD(cmd, arguments_exec);
             free(cmd);
             free_StingArrayArray(arguments_exec, taille);
-            return ;
+            return;
         }
     }
 }
 
-void recherche_commande_interne(char ** tab,int *last_exit,int taille)
+void recherche_commande_interne(char **tab, int *last_exit, int taille)
 {
     if (strcmp("exit", tab[0]) == 0)
     {
@@ -213,13 +217,11 @@ void recherche_commande_interne(char ** tab,int *last_exit,int taille)
     }
     else
     {
-        commande_externe(tab,taille);
+        commande_externe(tab, taille);
     }
 }
 
-
-
-void formatage_couleur(int last_exit,char *prompt,char *prompt_exit)
+void formatage_couleur(int last_exit, char *prompt, char *prompt_exit)
 {
     char *rouge = "\033[0;31m";
     char *vert = "\033[0;32m";
@@ -272,4 +274,3 @@ char *truncate_prompt(char *prompt, int max_size)
     }
     return res;
 }
-
