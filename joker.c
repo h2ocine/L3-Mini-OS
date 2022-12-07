531bb4a9d901fed06bb366067df0c77fd6c4de2e
kadem <hocine@DESKTOP-6KCKHON.localdomain>
2022-12-07 02:37:23 +0100

    mise a jour du joker etoile

--- joker.c
+++ joker.c
@@ -1,69 +1,65 @@
 #include "fonctions_auxiliaires.c"
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <dirent.h>
-#include <signal.h>
+{
+    int i = 0;
+    int size = 0;
+    char *s = NULL;
+    char **res = NULL;
 
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <limits.h>
-#include <stdlib.h>
+    // res[0]  = malloc(1*sizeof(char));
+    // res[0][0] = '\0';
 
-#include <sys/types.h>
-#include <sys/wait.h>
+    // Cas chaine vide
+    if (strlen(str) == 0)
+    {
+        *taille = 0;
+        return NULL;
+    }
 
-// retourne 1 si la chaine est valide en tant que balise, 0 sinon
+    // Séparer la chaine en plusieurs sous chaines :
+    char *strToken = strtok(str, separators);
+    while (strToken != NULL)
+    {
+        // On copie strToken dans une chaine de caractère s (pour avoir utiliser la taille exact)
+        if (!(s = malloc(strlen(strToken) + 1)))
+            perror("malloc");
+        if (snprintf(s, strlen(strToken) + 1, "%s", strToken) < 0)
+        {
+            perror("explode snprintf error ");
+            exit(1);
+        }
+        s[strlen(strToken)] = '\0';
+        // On ajoute la chaine de caractere s au tableau res
+        size += 1;
+        res = realloc(res, size * sizeof(char *)); // +1
+
+        if (res == NULL)
+            perror("fonction explode : realloc erreur ");
+
+        res[i] = s;
+        // res[i+1][] = '\0';
+        i++;
+
+        // On demande le token suivant.
+        strToken = strtok(NULL, separators);
+    }
+
+    if (!s)
+        free(s);
+    free(strToken);
+
+    *taille = i; // ici on retourne la taille de res
+    return res;
+}
+
 char is_valid(char *chaine)
 {
     if (chaine == NULL)
         return 0;
 
-    if (chaine[0] == '.')
-        return 0;
+    if (chaine[0] == '*')
+        return 1;
 
-    for (int i = 0; i < strlen(chaine); i++)
-    {
-        if (chaine[i] == '*')
-        {
-            if (i == strlen(chaine) - 1) // chaine est sous la forme prefix*
-                return 1;
-
-            if (chaine[i + 1] != '.') // si on a un autre charactère que '.' après * -> return 0
-                return 0;
-
-            for (int j = i + 2; j < strlen(chaine); j++) // si il y'a deux . dans le suffix -> return 0
-                if (chaine[j] == '.')
-                    return 0;
-
-            return 1;
-        }
-    }
-    return 0; // pas de '*' dans la chaine
-}
-
-char *get_prefix(char *chaine)
-{
-    char *resultat = malloc(1);
-    resultat[0] = '\0';
-    int compteur = 1;
-    for (int i = 0; strlen(chaine); i++)
-    {
-        if (chaine[i] == '*')
-            break;
-
-        compteur++;
-        resultat = realloc(resultat, compteur);
-        resultat[i] = chaine[i];
-        resultat[i + 1] = '\0';
-    }
-
-    return resultat;
+    return 0; 
 }
 
 char *get_postfix(char *chaine)
@@ -88,8 +84,8 @@ char *get_postfix(char *chaine)
     return resultat;
 }
 
-// verifie si pre est prefix de chaine (1 si oui; 0 sinon)
-int is_prefix(char *chaine, char *pre)
+// // verifie si pre est postfix de chaine (1 si oui; 0 sinon)
+int is_postfix(char *chaine, char *pre)
 {
     if (strcmp(pre, "") == 0)
         return 1;
@@ -97,68 +93,89 @@ int is_prefix(char *chaine, char *pre)
     if (strlen(pre) > strlen(chaine))
         return 0;
 
-    for (int i = 0; i < strlen(pre); i++)
-        if (chaine[i] != pre[i])
+    if(chaine[0] == '.') 
+        return 0;
+
+    int index_pre = strlen(pre) - 1;
+    int index_chaine = strlen(chaine) - 1;
+    while(index_pre >= 0)
+    {
+        if(pre[index_pre] != chaine[index_chaine])
             return 0;
 
+        index_pre--;
+        index_chaine--;
+    }
     return 1;
 }
 
-// // verifie si pre est postfix de chaine (1 si oui; 0 sinon)
-int is_postfix(char *chaine, char *pre)
+int nombre_mot_char(char *chaine, char separator)
 {
-    if (strcmp(pre, "") == 0)
-        return 1;
-
-    if (strlen(pre) > strlen(chaine) || strlen(chaine) == 1)
+    if (strlen(chaine) == 0)
         return 0;
 
-    for (int i = 1; i < strlen(chaine); i++)
-        if (chaine[i] == '.')
-        {
-            if (strlen(pre) != strlen(chaine) - i)
-                return 0;
+    int resultat = 1;
+    for (int i = 0; i < strlen(chaine); i++)
+        if (chaine[i] == 0)
+            resultat++;
 
-            for (int j = 0; j < strlen(pre); j++)
-                if (chaine[i + j] != pre[j])
-                    return 0;
-
-            return 1;
-        }
-
-    return 0;
+    return resultat;
 }
 
-
-int joker_1(char* commande, char* argument_commande, char* nom_dossiers_etoile)
+//retourne 0 en cas d'echec
+int joker_1(char* commande, char* argument_commande)
 {
-    // commande = "a*.c";
-    // argument_commande = "ls";
-    // nom_dossiers_etoile = "";
+    char commande[] = "*.c";
+    char argument_commande[] = "ls -a -l";
 
-    if (!is_valid(argv[1]))
-    {
-        printf("not valide\n");
-        exit(0);
-    }
+    //initialiser arguments avec commande + argument_commande
+    char* arguments = malloc(4000);
+    strncpy(arguments,commande,strlen(commande));
+    arguments[strlen(commande)] = ' ';
+    arguments[strlen(commande) + 1] = '\0';
+    strncat(arguments,argument_commande,strlen(argument_commande));
+    arguments[strlen(commande) + 1 + strlen(argument_commande)] = '\0';
+
+    printf("\narguments : %s\n", arguments);
+
+    if(!is_valid(commande))
+        return 0;
 
     DIR *repertoir = opendir(".");
     struct dirent *parcours;
+    int boolean = 0;
     while ((parcours = readdir(repertoir)))
     {
-        if (is_prefix(parcours->d_name, get_prefix(nom_dossiers_etoile)) == 1 && is_postfix(parcours->d_name, get_postfix(nom_dossiers_etoile)) == 1)
+        if (is_postfix(parcours->d_name, get_postfix(commande)) == 1)
         {
-            if (strcmp(argument_commande,"") != 0)
-            {
-                char *tmp[4] = {commande, argument_commande, parcours->d_name, NULL};
-                execCMD(commande, tmp);
-            }
-            else
-            {
-                char *tmp[3] = {commande, parcours->d_name, NULL};
-                execCMD(commande, tmp);
-            }
+            boolean = 1;
+            //ajouter le nom du fichier dans arguments
+            int strlen_arguments = strlen(arguments);
+            arguments[strlen_arguments] = ' ';
+            arguments[strlen_arguments + 1] = '\0';
+            strncat(arguments,parcours->d_name,strlen(parcours->d_name));
+            arguments[strlen_arguments + 1 + strlen(parcours->d_name)] = '\0';
         }
     }
-    return 0;
+    printf("arguments + fichiers : %s\n", arguments);
+    if(boolean)
+    {
+        int nbmot = nombre_mot_char(arguments,' ');
+        char **tmp = explode(arguments," ", &nbmot);
+        tmp[nbmot] = NULL;
+        printf("%d\n",nbmot);
+        
+        int i=0;
+        while(tmp[i] != NULL)
+        {
+             printf("%s\n",tmp[i]);i++;
+        }
+
+        free(arguments);
+        execCMD(argv[2], tmp);
+    }
+    else
+        free(arguments);
+
+    return 1;
 }
