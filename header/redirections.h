fb7d410dd9d9d73c994d4bd63e29f976fb26c22b
Thibault Fourmont <thibault75012@gmail.com>
2023-01-07 12:31:31 +0100

    redirections

--- header/redirections.h
+++ header/redirections.h
@@ -13,7 +13,19 @@
 
 
 
-
 //fonction qui check quel type de redirection
 int check_redirection(char **tab,int taille,int *last_exit);
+
+//Fonction qui "explode" le tableau de la ligne mais retire ce qui suit par exemple [ls,-l,>,fic.txt] -> [ls,-l]
+char **explode_redirection(char **tab, int taille);
+
+//fonction qui decoupe la commande et ses arguments, semblable à quelque chose pret à celle de cmdexterne.
+void decoupe_commande(char **tab, int taille, int *last_exit, char *fic, char *typeredirection);
+
+char **explode_redirection(char **tab, int taille);
+//fonction qui permet de savoir quelle fonction faire 
+void whichExec(char *cmd, char **args, int *last_exit, char *typeredirection,char *fic);
+
+//fonction pour la redirection sortie standard vers un fichier , ex ls -l > test.txt
+void execSORTIE(char *cmd, char **args, int *last_exit, char *fic);
 
