9eaedc8e2b5512adda302f609e60daa9766b3b90
Thibault Fourmont <thibault75012@gmail.com>
2023-01-08 20:07:05 +0100

    redirection cmd intern

--- main/redirections.c
+++ main/redirections.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 
 const char *redirections[] = {">", "<", ">|", ">>", "2>", "2>|", "2>>"};
+const char *cmd_interne[] = {"pwd", "exit", "cd"};
 
 int compte_nombre_pipe(char **tab_clean, int taille_tabclean)
 {
@@ -19,6 +20,18 @@ int compte_nombre_pipe(char **tab_clean, int taille_tabclean)
         }
     }
     return count;
+}
+
+char **explode_redirection(char **tab, int taille)
+{
+    char **newtab = NULL;
+    for (int i = 0; i < taille - 2; i++)
+    {
+        newtab = realloc(newtab, strlen(tab[i]));
+        newtab[i] = tab[i];
+    }
+
+    return newtab;
 }
 
 void good_flags_descriptor(char *typeredirection, int *flags, int *descriptor)
@@ -60,7 +73,50 @@ void good_flags_descriptor(char *typeredirection, int *flags, int *descriptor)
     }
 }
 
-void redirect_cmd(char *cmd, char **args, int *last_exit, char *fic, char *typeredirection)
+
+
+void good_flags_descriptor_interne(char *typeredirection, int *flags, int *descriptor)
+{
+    if (strcmp(typeredirection, ">") == 0)
+    {
+        *flags = O_WRONLY | O_CREAT | O_EXCL;
+        *descriptor = STDERR_FILENO;
+    }
+    else if (strcmp(typeredirection, "<") == 0)
+    {
+        *flags = O_RDONLY;
+        *descriptor = STDIN_FILENO;
+    }
+    else if (strcmp(typeredirection, ">|") == 0)
+    {
+        *flags = O_WRONLY | O_CREAT | O_TRUNC | O_NOFOLLOW;
+        *descriptor = STDERR_FILENO;
+    }
+    else if (strcmp(typeredirection, ">>") == 0)
+    {
+        *flags = O_APPEND | O_CREAT | O_WRONLY | O_NOFOLLOW;
+        *descriptor = STDERR_FILENO;
+    }
+    else if (strcmp(typeredirection, "2>") == 0)
+    {
+        *flags = O_WRONLY | O_CREAT | O_EXCL | O_NOFOLLOW;
+        *descriptor = STDERR_FILENO;
+    }
+    else if (strcmp(typeredirection, "2>|") == 0)
+    {
+        *flags = O_WRONLY | O_CREAT | O_TRUNC | O_NOFOLLOW;
+        *descriptor = STDERR_FILENO;
+    }
+    else if (strcmp(typeredirection, "2>>") == 0)
+    {
+        *flags = O_APPEND | O_CREAT | O_WRONLY | O_NOFOLLOW;
+        *descriptor = STDERR_FILENO;
+    }
+}
+
+ 
+
+void redirect_cmd_ext(char *cmd, char **args, int *last_exit, char *fic, char *typeredirection)
 {
 
     int *flags = malloc(sizeof(int) * 10);
@@ -111,6 +167,126 @@ void redirect_cmd(char *cmd, char **args, int *last_exit, char *fic, char *typer
     }
 }
 
+void exec_interne(char **tab, int *last_exit, int taille)
+{
+    // On met tout les chemins mentionnées par l'utilisateur dans un tableau; ls * *.c -> ["*", "*.c"]
+    printf("exec_interne \n");
+    char **all_path = NULL;
+    int size_path = 0;
+    // printf(" ca rentre \n");
+    for (int i = 1; i < taille; i++)
+    {
+        if (tab[i][0] != '-')
+        {
+            size_path++;
+            all_path = realloc(all_path, sizeof(char *) * size_path);
+            all_path[size_path - 1] = malloc(strlen(tab[i]) + 1);
+            snprintf(all_path[size_path - 1], strlen(tab[i]) + 1, "%s", tab[i]);
+            all_path[size_path - 1][strlen(tab[i])] = '\0';
+        }
+    }
+
+    int size_all_joker;
+
+    char **all_joker = all(all_path, size_path, "", &size_all_joker);
+    free_StingArrayArray(all_path, size_path);
+
+    if (strcmp("exit", tab[0]) == 0)
+    {
+        if (taille >= 2)
+        {
+            if (size_all_joker > 1)
+            {
+                printf("Erreur dans exit\n");
+                free_StingArrayArray(all_joker, size_all_joker);
+                return;
+            }
+            char t[strlen(tab[1]) + 1];
+            snprintf(t, strlen(tab[1]) + 1, "%s", tab[1]);
+            t[strlen(tab[1]) + 1] = '\0';
+            free_StingArrayArray(tab, taille);
+
+            free_StingArrayArray(all_joker, size_all_joker);
+            *last_exit = exits(t, *last_exit);
+        }
+        else
+        {
+            free_StingArrayArray(all_joker, size_all_joker);
+            *last_exit = exits(NULL, *last_exit);
+        }
+    }
+    else if (strcmp("cd", tab[0]) == 0)
+    {
+        if (size_all_joker > 1)
+        {
+            printf("Erreur dans exit\n");
+            free_StingArrayArray(all_joker, size_all_joker);
+            return;
+        }
+
+        char *arg;
+        char *ref;
+        if (taille == 1)
+        {
+            arg = NULL;
+            ref = NULL; //*//
+        }
+        else if (taille == 2)
+        {
+            arg = NULL;
+            ref = tab[1];
+        }
+        else
+        {
+            arg = tab[1];
+            ref = tab[2];
+        }
+
+        *last_exit = cd(arg, ref);
+    }
+    else if (strcmp("pwd", tab[0]) == 0)
+    {
+        printf("ca rentre dans le pwd\n");
+        *last_exit = pwd(taille, tab);
+    }
+}
+
+void redirect_cmd_interne(char **tab, int taille, int *last_exit, char *fic, char *typeredirection)
+{
+
+    int *flags = malloc(sizeof(int) * 10);
+    int *descriptor = malloc(sizeof(int) * 10);
+
+    good_flags_descriptor(typeredirection, flags, descriptor);
+
+
+    int status;
+    int t = 0;
+
+    // Crée un processus fils
+
+    // Dans le processus fils :
+    // Redirige l'entrée standard de la commande vers le fichier ou le tube nommé
+    int fd = open(fic, *flags, 0644);
+
+
+    if (dup2(fd, *descriptor) == -1)
+    {
+        perror("Erreur lors de la redirection de l'entrée standard de la commande");
+        exit(1);
+    }
+
+    
+    // Exécute la commande
+    exec_interne(tab, last_exit, taille);
+
+    close(fd);
+    free(flags);
+    free(descriptor);
+
+    exit(1);
+}
+
 void cut_cmd_redirec(char **tab, int taille, int *last_exit, char *fic, char *typeredirection)
 {
     char pre[3];
@@ -156,7 +332,7 @@ void cut_cmd_redirec(char **tab, int taille, int *last_exit, char *fic, char *ty
             }
             arguments_exec[taille] = NULL;
             // execCMD(cpyPwd, arguments_exec, last_exit);
-            redirect_cmd(cpyPwd, arguments_exec, last_exit, fic, typeredirection);
+            redirect_cmd_ext(cpyPwd, arguments_exec, last_exit, fic, typeredirection);
             free_StingArrayArray(arguments_exec, taille);
         }
         else
@@ -239,7 +415,7 @@ void cut_cmd_redirec(char **tab, int taille, int *last_exit, char *fic, char *ty
                 arguments_exec[taille] = NULL;
 
                 // Fait la redirection qui correspond au "typeredirection"
-                redirect_cmd(cmd, arguments_exec, last_exit, fic, typeredirection);
+                redirect_cmd_ext(cmd, arguments_exec, last_exit, fic, typeredirection);
 
                 free(cmd);
                 free_StingArrayArray(arguments_exec, taille);
@@ -253,44 +429,56 @@ void cut_cmd_redirec(char **tab, int taille, int *last_exit, char *fic, char *ty
     }
 }
 
-char **explode_redirection(char **tab, int taille)
-{
-    char **newtab = NULL;
-    for (int i = 0; i < taille - 2; i++)
-    {
-        newtab = realloc(newtab, strlen(tab[i]));
-        newtab[i] = tab[i];
-    }
-
-    return newtab;
-}
-
-
-
-int is_pipe_redirection(char **tab, int taille,int *last_exit)
+int check_cmd_intern(char **tab, int taille)
 {
     for (int i = 0; i < taille; i++)
     {
-        if (strcmp(tab[i], "|") == 0)
+        for (int j = 0; j < 3; j++)
         {
-            printf("pipe \n");
-            return 1;
-        }
-        else
-        {
-            for (int j = 0; j < 7; j++)
+            if (strcmp(tab[i], cmd_interne[j]) == 0)
             {
-                if (strcmp(tab[i], redirections[j]) == 0)
-                {
-                    char **tab_clean = explode_redirection(tab, taille);
-                    int taille_tabclean = taille - 2;
-                    cut_cmd_redirec(tab_clean, taille_tabclean, last_exit, tab[taille - 1], tab[taille - 2]);
-                    return 1;
-                }
+                return 1;
             }
         }
     }
     return 0;
 }
 
+int verif_redirection(char **tab, int taille)
+{
+    for (int i = 0; i < taille; i++)
+    {
+        for (int j = 0; j < 7; j++)
+        {
+            if (strcmp(tab[i], redirections[j]) == 0)
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
 
+// Fonction qui va dispatcher les différents cas
+int check_redirection(char **tab, int taille, int *last_exit)
+{
+    // On verifie si il y a bien une redirection
+    if (verif_redirection(tab, taille) == 1)
+    {
+        if (check_cmd_intern(tab, taille) == 1)
+        {
+            redirect_cmd_interne(tab, taille, last_exit, tab[taille - 1], tab[taille - 2]);
+            return 1;
+        }
+        else
+        {
+            char **tab_clean = explode_redirection(tab, taille);
+            int taille_tabclean = taille - 2;
+            cut_cmd_redirec(tab_clean, taille_tabclean, last_exit, tab[taille - 1], tab[taille - 2]);
+            return 1;
+        }
+    }
+    // On parcourt le tableau des redirections pour voir si il y en a une
+
+    return 0;
+}
